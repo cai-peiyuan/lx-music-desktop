d6d0c13b64a7837271b3359fc5973054f863f2e7
校长 <262114450@qq.com>
2023-02-09 11:21:25 +0800

    地图页面测试

---
+++ src/renderer/views/abcmapgl/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="asd" :class="$style.container">
+    abcmapgl啊啊asdasd
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute, useRouter } from '@common/utils/vueRouter'
+import { ref, computed, nextTick, onMounted } from '@common/utils/vueTools'
+import maplibregl from 'maplibre-gl'
+
+const route = useRoute()
+const router = useRouter()
+console.log('setup', maplibregl)
+
+onMounted(() => {
+  console.log('onMounted', maplibregl)
+  const map = new maplibregl.Map({
+    container: 'asd',
+    style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+  })
+})
+
+</script>
+
+<style lang="less" module>
+.container {
+  border: red solid 2px;
+}
+
+.header {
+  // padding: 5px 0;
+  flex: none;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+}
+
+.main {
+  position: relative;
+  flex: auto;
+  // min-height: 0;
+}
+</style>
